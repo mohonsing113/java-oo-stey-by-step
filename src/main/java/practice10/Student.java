efd67b66d11d361a30393866cf17677c804f2bd7
Mo Hon Sing <h.s.mo@oocl.com>
2018-11-13 13:18:02 +0800

    finish pratice10

--- src/main/java/practice10/Student.java
+++ src/main/java/practice10/Student.java
@@ -1,4 +1,22 @@
 package practice10;
 
-public class Student {
+public class Student extends Person {
+    Klass klass;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        if (klass.leader == this) {
+            return String.format(super.introduce() + " I am a Student. I am Leader of " + getKlass().getDisplayName() + ".");
+        } else {
+            return String.format(super.introduce() + " I am a Student. I am at " + getKlass().getDisplayName() + ".");
+        }
+    }
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,50 @@
 package practice10;
 
-public class Teacher {
+import java.util.*;
+
+public class Teacher extends Person {
+    LinkedList<Klass> classes;
+
+    public Teacher(int id, String name, int age, LinkedList<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public String introduce() {
+        if (classes == null || classes.size() == 0) {
+            return String.format(super.introduce() + " I am a Teacher. I teach No Class.");
+        }
+        return String.format(super.introduce() + " I am a Teacher. I teach %s.", buildClassDisplayName());
+    }
+
+    public String buildClassDisplayName() {
+
+        String display = "Class ";
+        for (int i = 0; i < classes.size(); i++) {
+            display += classes.get(i).getNumber();
+            if(i!=classes.size()-1){
+                display+=", ";
+            }
+        }
+        return display;
+    }
+
+    public String introduceWith(Student student) {
+        if (isTeaching(student)) {
+            return String.format("My name is %s. I am %s years old. I am a Teacher. I teach %s.", name, age, student.getName());
+        } else {
+            return String.format("My name is %s. I am %s years old. I am a Teacher. I don't teach %s.", name, age, student.getName());
+        }
+    }
+
+    public LinkedList<Klass> getClasses() {
+        return classes;
+    }
+    public boolean isTeaching(Student student){
+        return classes.contains(student.getKlass());
+    }
 }
